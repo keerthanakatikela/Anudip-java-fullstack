fe6e5ce7a92aa77b6d8b35a5861a0f4077dc0fee
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 20:03:44 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package firstproject;
+// Write a program to check a year is leap year or not.
+public class LeapYear {
+      public static void main(String [] args)
+      {
+    	  int year=2024;
+    	  if(year%4 == 0)
+    	  {
+    		  System.out.println("It is a leap year");
+    	  }
+    	  else
+    	  {
+    		  System.out.println("It is not a leap year");
+    	  }
+      }
+}
