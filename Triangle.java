85d56c2e7a8e44c1748f3402f368ff0bab6412d3
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 19:53:31 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+package firstproject;
+// Write a program to input any three angles and check they form a triangle or not.
+public class Triangle {
+     public static void main(String[] args)
+     {
+    	 int a=90;
+    	 int b=20;
+    	 int c=70;
+    	 int d=a+b+c;
+    	 if(d==180)
+    	 {
+    		 System.out.println("It is a triangle");
+    	 }
+    	 else
+    	 {
+    		 System.out.println("It is not a triangle");
+    	 }
+     }
+}
