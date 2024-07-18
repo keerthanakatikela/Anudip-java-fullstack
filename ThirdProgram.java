8c9fec10fd9f153e92f523005f76e19f524d4ef9
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 19:32:08 +0530

    Add files via upload
    1. Write a java program to display any 10 sentence about yourself.
    2. Write a java program to print any 10 sentence about computer
    3. Write a java program to print any 15 sentence to describe architecture of JVM

---
+++ ThirdProgram.java
@@ -0,0 +1,23 @@
+package firstproject;
+
+public class ThirdProgram {
+    public static void main(String [] args)
+    {
+    	//To display 15 sentences to describe about the architecture of JVM
+    	System.out.println("1.JVM stands for Java virtual machine.");
+    	System.out.println("2.It is a software-based engine.");
+    	System.out.println("3.It is necessary for running java bytecode applications.");
+    	System.out.println("4.The 3 subsections in JVM are: 1.Class loader,2.Runtime Data Areas,3.Execution engine.");
+    	System.out.println("5.Class loader is responsible for loading,linking,initialisation.");
+    	System.out.println("6.Three different class loaders are bootstrap,extension,application.");
+    	System.out.println("7.Bootstrap classloader is a root class loader .");
+    	System.out.println("8.Extension class loader is a subclass of bootstrap classloader.");
+    	System.out.println("9.Application classloader is a final classloader.");
+    	System.out.println("10.Linking involves verification,preparation and resolving.");
+    	System.out.println("11.Runtime data areas include method area,heap area,stack area,pc registers amd native method stacks.");
+    	System.out.println("12.Execution engine includes interpreter,JIT compiler and Garbage collector.");
+    	System.out.println("13.JVM helps programmers to write code systematically.");
+    	System.out.println("14.It is responsible for loading file that has been written and compile into (.classfile)");
+    	System.out.println("15.JVM is implemented through the JRE(Java Runtime Environment)");
+    }
+}
