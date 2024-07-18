6bf95a69629d3d8cd74a1d884dab33a90213e4d9
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 19:51:38 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,12 @@
+package firstproject;
+// Write a java program to calculate simple interest.
+public class SimpleInterest {
+  public static void main(String[] args)
+  {
+	  int p=1000;
+	  int r=10;
+	  int t=2;
+	  double simpleInterest=p*r*t/100;
+	  System.out.println(simpleInterest);
+  }
+}
