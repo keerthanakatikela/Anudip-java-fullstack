11a458c3f79fb00630da66e8ff1aa5fd7d196253
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 19:42:14 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,19 @@
+package firstproject;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		//To display 10 sentences about myself
+		System.out.println("1.My name is Keerthana.");
+		System.out.println("2.I am native to Shadnagar.");
+		System.out.println("3.Currently I am pursuing MCA. ");
+		System.out.println("4.I am a Quick learner.");
+		System.out.println("5.I belong to a nuclear family.");
+		System.out.println("6.I have two siblings .");
+		System.out.println("7.I am good at programming in c and java.");
+		System.out.println("8.My hobbies are listening music and Dancing.");
+		System.out.println("9.My short term goal is to get job in a MNC Companies.");
+		System.out.println("10.My long term goal is to be in a good position and upgrade my skills for better growth in my career.");
+	}
+
+}
