e5ac004a9fde57b9ae9e213c4ea431fad9a30b80
keerthanakatikela <keerthanakatikela@gmail.com>
2024-07-18 19:52:31 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,10 @@
+package firstproject;
+// write a java from to input  temperature in Fahrenheit and convert it in Celsius.
+public class Temperature {
+  public static void main(String [] args)
+  {
+	  int tf=100;
+	  int tc=(tf-32)*5/9;
+	  System.out.println("The temperature in celsius is : " + tc);
+  }
+}
